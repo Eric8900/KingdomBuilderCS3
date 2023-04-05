27d8a2e332c6bde87f8fdd8b3494701ccc9e1967
Fiszic <zhengyechen2018@gmail.com>
2023-04-05 07:44:57 -0500

    Deck Class

---
+++ Logic1/Deck.java
@@ -0,0 +1,61 @@
+package Logic1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck {
+    private ArrayList<Integer> deck;
+    private ArrayList<Integer> discard;
+    private ArrayList<Integer> objectiveCards;
+
+    //Deck constructor. Instantiates instance variables
+    public Deck(){
+        deck = new ArrayList<>();
+        discard = new ArrayList<>();
+        objectiveCards = new ArrayList<>();
+        for(int i = 0; i<5; i++){
+            for(int j = 0; j<5; j++){
+                deck.add(i);
+            }
+        }
+        shuffleDeck();
+        for(int x = 0; x<10; x++){
+            objectiveCards.add(x);
+        }
+        shuffleObjectiveCards();
+    }
+
+    //Shuffles the deck
+    public void shuffleDeck(){Collections.shuffle(deck);}
+
+    //Shuffles the objective cards
+    public void shuffleObjectiveCards(){Collections.shuffle(objectiveCards);}
+
+    //Refills the deck with discarded cards
+    public void refillDeck(){
+        deck.addAll(discard);
+        discard.clear();
+        shuffleDeck();
+    }
+
+
+    //Accessor methods
+
+    //returns the deck arraylist
+    public ArrayList<Integer> getDeck(){return deck;}
+
+    //returns the objective cards
+    public ArrayList<Integer> getObjectiveCards(){return objectiveCards;}
+
+    //returns the discard deck
+    public ArrayList<Integer> getDiscard(){return discard;}
+
+    //returns the top terrain card and removes it from the deck
+    public Integer getTerrainCard(){return deck.remove(0);}
+
+
+    //Modifier methods
+
+    //Adds a discarded card to the discard deck
+    public void discard(int card){discard.add(card);}
+}
