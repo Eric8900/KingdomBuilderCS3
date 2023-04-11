8d9ee2a63b922c3a35d901ff1cfeec59e61ab193
Dosunmu <K1211713@students.katyisd.org>
2023-04-11 08:05:02 -0500

    scoring

--- Logic1/PostGame.java
+++ Logic1/PostGame.java
@@ -1,5 +1,54 @@
 package Logic1;
+import java.io.*;
+import java.util.*;
 
 public class PostGame {
-    
+    public ArrayList<Integer> score;
+    public ArrayList<Integer> objCards;
+    public int currPlayer;
+    public int currObj;
+    public PostGame(ArrayList<Integer> score, ArrayList<Integer> objCards,int currPlayer, int currObj){
+        this.score = score;
+        this.objCards = objCards;
+        this.currPlayer = currPlayer;
+        this.currObj = currObj;
+    }
+
+    public void scorePlayers(){
+
+    }
+    public void scoreFishermen(){
+
+    }
+    public void scoreMerchants(){
+
+    }
+    public void scoreDiscovers(){
+
+    }
+    public void scoreHermits(){
+
+    }
+    public void scoreCitizens(){
+
+    }
+    public void scoreMiners(){
+
+    }
+    public void scoreWorkers(){
+
+    }
+    public void scoreKnights(){
+
+    }
+    public void scoreLords(){
+
+    }
+    public void scoreFarmers(){
+
+    }
+    public void dfs(){
+
+    }
+
 }
