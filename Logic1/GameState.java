1876ede920e4d1e96c367c4eadfac3009cc12567
Fiszic <zhengyechen2018@gmail.com>
2023-04-23 00:41:42 -0500

    Force-end Button for Fikky To Test EndGame

--- Logic1/GameState.java
+++ Logic1/GameState.java
@@ -10,7 +10,7 @@ public class GameState {
 	public static ArrayList<Integer> objCards;
 	public static Sector[] sectors = new Sector[4];
 	public enum State {
-		DRAWCARD, PLAYSETTLEMENTS, PLAYADDLOCATIONTILE, PLAYMOVELOCATIONTILE, MOVELOCATIONTILE, NEXTTURN, MAINMENU
+		DRAWCARD, PLAYSETTLEMENTS, PLAYADDLOCATIONTILE, PLAYMOVELOCATIONTILE, MOVELOCATIONTILE, NEXTTURN, MAINMENU, ENDGAME
 	}
 	public GameState() {
 		players = new ArrayList<Player>();
